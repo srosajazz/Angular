d2dcad6cdbb5ee97ed8d7f93240e63300d0f64df
Sergio Rosa <scarlosjazz@gmail.com>
2019-11-23 14:23:47 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "sr-root",
+  templateUrl: "./app.component.html"
+})
+export class AppComponent {
+  title = "sergio-academy";
+}
